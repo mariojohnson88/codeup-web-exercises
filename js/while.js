8e32e5fd6bd10236a8786f93c1a109a0a08118a1
Mario Johnson <marioj@Marios-MacBook-Pro.local>
2019-06-19 10:10:47 -0500

    Completed while loops JS exercise

--- js/while.js
+++ js/while.js
@@ -4,3 +4,21 @@ while(i < 65536) {
     i = i * 2;
     console.log(i);
 }
+
+
+
+var allCones = Math.floor(Math.random() * 50) + 50;
+
+do {
+    var conesSold = Math.floor(Math.random() * 5) + 1;
+
+    if (allCones < conesSold) {
+        console.log("Customer attempted to buy too many cones");
+
+    } else {
+        allCones -= conesSold;
+        console.log("Customer bought " + conesSold);
+    }
+
+} while (allCones >= 1 );
+
