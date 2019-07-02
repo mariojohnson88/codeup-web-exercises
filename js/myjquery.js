a8e36b423c4a4c35cec204e0228e0b015dd8cfd9
Mario Johnson <marioj@Marios-MacBook-Pro.local>
2019-07-02 14:14:39 -0500

    Completed jQuery Exercise

--- js/myjquery.js
+++ js/myjquery.js
@@ -7,16 +7,18 @@
 // OR
 // alert($('#second-paragraph').html());
 
+
+// Class selectors
+
 // This originally had the same ID of "generic-lists" set to 2 different elements. What ended up happening is that jquery only took the first occurrence of the id, not the second.
+
 // // // var duplicateId = $('#generic-lists').html();
 // // // alert(duplicateId);
-
-
-
 // $('.codeup').css('border', '1px solid red');
 
-$('li').css('font-size', '20px').css('background-color', 'red');
 
+// Element selectors
+
+$('li').css('font-size', '20px');
 $('h1, p, li').css('background-color', '#ef0');
-
 alert($('h1').html());
