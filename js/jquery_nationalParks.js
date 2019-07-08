8354f3549de469fa17e298756660bd73b6379c44
Mario Johnson <marioj@Marios-MacBook-Pro.local>
2019-07-08 14:19:02 -0500

    Completed jQuery effects exercise

--- js/jquery_nationalParks.js
+++ js/jquery_nationalParks.js
@@ -13,8 +13,9 @@ $(document).ready(function() {
             $(this).children().last().css('background-color', '#FFFF3F')
         })
     });
-
+    $("ul").hide();
     $('h3').click(function () {
+        $(this).next().slideToggle(2500);
             $(this).next().children().css('font-weight', 'bold')
     //    here you could also leave out .children and it would yield the same results
     });
@@ -23,11 +24,15 @@ $(document).ready(function() {
         $(this).parent().children().first().css('color', 'blue')
     });
 
+    $('span').click(function() {
+        $('div').hide()
+    });
 
-
-
-
-
+    $('#register').hide();
+    var delay = 8000;
+    var timeoutId = setTimeout(function () {
+        $('#register').fadeIn(4000);
+    }, delay);
 
 
 
