f1bde8689e6b167ff451581af221e6397e5ceeb3
Mario Johnson <marioj@Marios-MacBook-Pro.local>
2019-07-08 11:32:49 -0500

    Completed traversing exercise

--- js/jquery_nationalParks.js
+++ js/jquery_nationalParks.js
@@ -6,13 +6,22 @@ $(document).ready(function() {
     });
     $('dt').click(function() {
         $(this).toggleClass('highlight');
-    })
+    });
 
+    $('#highlight-last-fact').click(function() {
+        $('ul').each(function () {
+            $(this).children().last().css('background-color', '#FFFF3F')
+        })
+    });
 
+    $('h3').click(function () {
+            $(this).next().children().css('font-weight', 'bold')
+    //    here you could also leave out .children and it would yield the same results
+    });
 
-
-
-
+    $('li').click(function() {
+        $(this).parent().children().first().css('color', 'blue')
+    });
 
 
 
