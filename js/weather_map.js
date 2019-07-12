e138eaaf80a5c556c8aca6d870e24eae09d64e6f
Mario Johnson <johnson.mario8404@gmail.com>
2019-07-12 16:31:16 -0500

    Updated weather map

---
+++ js/weather_map.js
@@ -0,0 +1,12 @@
+var iconObjects = [
+    {condition: "clear-day", url: ""},
+    {condition: "clear-night", url: ""},
+    {condition: "rain", url: ""},
+    {condition: "snow", url: ""},
+    {condition: "sleet", url: ""},
+    {condition: "wind", url: ""},
+    {condition: "fog", url: ""},
+    {condition: "cloudy", url: ""},
+    {condition: "partly-cloudy-day", url: ""},
+    {condition: "partly-cloudy-night", url: ""}
+    ];
