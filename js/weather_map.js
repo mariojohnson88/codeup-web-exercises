0446d43728d8dc1158da89f4c1b4bd884ba033f4
Mario Johnson <johnson.mario8404@gmail.com>
2019-07-12 19:12:11 -0500

    updated weather map

--- js/weather_map.js
+++ js/weather_map.js
@@ -1,12 +1,12 @@
 var iconObjects = [
-    {condition: "clear-day", url: ""},
-    {condition: "clear-night", url: ""},
-    {condition: "rain", url: ""},
-    {condition: "snow", url: ""},
-    {condition: "sleet", url: ""},
-    {condition: "wind", url: ""},
-    {condition: "fog", url: ""},
-    {condition: "cloudy", url: ""},
-    {condition: "partly-cloudy-day", url: ""},
-    {condition: "partly-cloudy-night", url: ""}
-    ];
+    {forecastSummary: "Clear", forecast: "Clear", condition: "clear-day", url: "climacons-master/SVG/Sun.svg"},
+    {forecastSummary: "Clear", forecast: "Clear", condition: "clear-night", url: "climacons-master/SVG/Moon.svg"},
+    {forecastSummary: "Rainy", forecast: "Rain", condition: "rain", url: "climacons-master/SVG/Cloud-Rain.svg"},
+    {forecastSummary: "Snowing", forecast: "Snow", condition: "snow", url: "climacons-master/SVG/Snowflake.svg"},
+    {forecastSummary: "Sleeting", forecast: "Sleet", condition: "sleet", url: "climacons-master/SVG/Cloud-Snow.svg"},
+    {forecastSummary: "Windy", forecast: "Wind", condition: "wind", url: "climacons-master/SVG/Wind.svg"},
+    {forecastSummary: "Foggy", forecast: "Fog", condition: "fog", url: "climacons-master/SVG/Cloud-Fog.svg"},
+    {forecastSummary: "Cloudy", forecast: "Clouds", condition: "cloudy", url: "climacons-master/SVG/Cloud.svg"},
+    {forecastSummary: "Partly Cloudy", forecast: "Clouds", condition: "partly-cloudy-day", url: "climacons-master/SVG/Cloud-Sun.svg"},
+    {forecastSummary: "Partly Cloudy", forecast: "Clouds", condition: "partly-cloudy-night", url: "climacons-master/SVG/Cloud-Moon.svg"}
+];
