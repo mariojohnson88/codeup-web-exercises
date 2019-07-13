eec839cc88c7d37040f1a307b23d5ed6ffa08f0c
Mario Johnson <johnson.mario8404@gmail.com>
2019-07-13 18:16:44 -0500

    updated weather map

--- js/weather_map.js
+++ js/weather_map.js
@@ -1,3 +1,6 @@
+(function () {
+"use strict";
+
 var iconObjects = [
     {forecastSummary: "Clear", forecast: "Clear", condition: "clear-day", url: "climacons-master/SVG/Sun.svg"},
     {forecastSummary: "Clear", forecast: "Clear", condition: "clear-night", url: "climacons-master/SVG/Moon.svg"},
@@ -10,3 +13,45 @@ var iconObjects = [
     {forecastSummary: "Partly Cloudy", forecast: "Clouds", condition: "partly-cloudy-day", url: "climacons-master/SVG/Cloud-Sun.svg"},
     {forecastSummary: "Partly Cloudy", forecast: "Clouds", condition: "partly-cloudy-night", url: "climacons-master/SVG/Cloud-Moon.svg"}
 ];
+
+mapboxgl.accessToken = mapboxAPI;
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v9'
+});
+
+$.get("https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/" + darkSkyAPI + "/29.4241,-98.4936").done(function(data){
+    console.log(data);
+    addCurrentConditions(data.daily.data);
+});
+
+function addCurrentConditions(data) {
+    var url;
+    var forecast;
+    var forecastSummary;
+    var html = "";
+    console.log(data);
+    data.forEach(function(condition, i) {
+        iconObjects.forEach(function (value) {
+            if (condition.icon === value.condition) {
+                url = value.url;
+                forecast = value.forecast;
+                forecastSummary = value.forecastSummary;
+            }
+        });
+        console.log(condition);
+        if(i <= 2) {
+            html += "<div class='col-4'>";
+            html += "<p>" + Math.round(condition.apparentTemperatureHigh) + "°/" + Math.round(condition.apparentTemperatureLow) + "°</p>";
+            html += "<img src='" + url + "'>";
+            html += "<p>" + "<strong>" + forecast + ": </strong>" + forecastSummary + "</p>";
+            html += "<p>" + "<strong>Humidity: </strong>" + Math.round(condition.humidity * 100) + "</p>";
+            html += "<p>" + "<strong>Wind: </strong>" + condition.windSpeed + "</p>";
+            html += "<p>" + "<strong>Pressure: </strong>" + condition.pressure + "</p>";
+            html += "</div>";
+        }
+    });
+    $('#weather').append(html)
+}
+
+})();
