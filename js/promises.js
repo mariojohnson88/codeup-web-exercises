28472b3b50d5483abea1b85b0405a1d2e7d2b5a7
Mario Johnson <johnson.mario8404@gmail.com>
2019-07-18 15:56:28 -0500

    Updated promises exercise

--- js/promises.js
+++ js/promises.js
@@ -1,7 +1,7 @@
-(function (){
-"use strict";
+(function () {
+    "use strict";
 
-function wait(number) {
+    function wait(number) {
         return new Promise((resolve, reject) => {
             setTimeout(() => {
                 if (number) {
@@ -15,4 +15,28 @@ function wait(number) {
 
     wait(1000).then(() => console.log('You\'ll see this after 1 second'));
     wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
+
+
+    const getLastPush = function(userName) {
+        const gitUrl = "https://api.github.com/users/" + userName + "/events";
+        return fetch(gitUrl, {
+            headers: {
+                'Authorization': `token ${gitHubAPI}`
+            }
+        })
+            .then(response => response.json())
+            .then(events => {
+                for (let event of events) {
+                    if (event.type === "PushEvent") {
+                        // console.log(`${event.type} at ${event.type.created_at}`)
+                        return (event.created_at);
+                    }
+                }
+            })
+    };
+
+    let userName = prompt('what is your user name?');
+console.log(getLastPush(userName))
+
+
 })();
